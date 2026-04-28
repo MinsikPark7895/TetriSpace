eb198e635b5c28a647df854533299eff1d1c3918
김제원 <1em0nbam@naver.com>
2026-04-28 17:35:53 +0900

    feat: object contouring vision test

--- src/dakae_vision/setup.py
+++ src/dakae_vision/setup.py
@@ -1,8 +1,18 @@
 import os
 from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'dakae_vision'
+model_files = [
+    (os.path.join('share', package_name, os.path.dirname(path)), [path])
+    for path in glob('best/**/*', recursive=True)
+    if os.path.isfile(path)
+]
+if os.path.isfile('best.pt'):
+    model_files.append((os.path.join('share', package_name), ['best.pt']))
+if os.path.isfile('dakae_v2_best.pt'):
+    model_files.append((os.path.join('share', package_name), ['dakae_v2_best.pt']))
 
 setup(
     name=package_name,
@@ -12,8 +22,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-    ],
+    ] + model_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='dakae',
@@ -28,9 +37,9 @@ setup(
     entry_points={
         'console_scripts': [
             'cube_service_server = dakae_vision.cube_service_server:main',
-            'marker_service_server = dakae_vision.marker_service_server:main',
-            'camera_node = dakae_vision.camera_node:main',
-            'marker_service_server_offset = dakae_vision.marker_service_server_offset:main',
+            'object_contour_service_server = dakae_vision.object_contour_service_server:main',
+            'collect_dataset = dakae_vision.collect_dataset:main',
+            'ppo_robot_controller = dakae_vision.ppo_robot_controller:main',
         ],
     },
 )
